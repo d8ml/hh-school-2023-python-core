c05200fc91afbc2854b7989909d547e5110adfb0
Ivanov Petr <d8ml@yandex.ru>
2024-01-08 19:15:15 +0500

    Add homework solution

--- hw/beer.py
+++ hw/beer.py
@@ -1,4 +1,10 @@
 class Beer:
     def __init__(self, title=None, production_date=None) -> None:
-        # TODO: добавить инициализацию
-        pass
+        self.title = title
+        self.production_date = production_date
+
+    def __str__(self):
+        return f'Beer: {self.title} at {self.production_date}'
+
+    def __repr__(self):
+        return str(self)
--- hw/main.py
+++ hw/main.py
@@ -1,3 +1,5 @@
+import time
+
 from wine import Wine
 from beer import Beer
 from market import Market
@@ -10,3 +12,13 @@ TODO: Доработать заготовки классов вина (Wine), п
     * получить список напитков (вина и пива) в указанном диапазоне даты производства
     * (*) написать свой декоратор, который бы логировал начало выполнения метода и выводил время выполнения
 """
+
+
+now = time.time()
+beers = [Beer("beer4"), Beer("beer2"), Beer("beer4", now), Beer("beer4", now + 5), Beer("beer45", now + 15)]
+wines = [Wine("awine1"), Wine("wine4")]
+market = Market(beers=beers, wines=wines)
+
+print(market.has_drink_with_title("beer4"))
+print(market.get_drinks_sorted_by_title())
+print(market.get_drinks_by_production_date(now - 12, now + 12))
